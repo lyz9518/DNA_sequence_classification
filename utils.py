593878158c5e3c5e63759dd1962d82a65db42145
Arthur Liu <lyz9518@163.com>
2021-12-19 02:08:25 -0500

    Add model and EDA

---
+++ utils.py
@@ -0,0 +1,10 @@
+def Kmers_funct(seq, size=6):
+    return [seq[x:x+size].lower() for x in range(len(seq) - size + 1)]
+
+
+def get_metrics(y_test, y_predicted):
+    accuracy = accuracy_score(y_test, y_predicted)
+    precision = precision_score(y_test, y_predicted, average='weighted')
+    recall = recall_score(y_test, y_predicted, average='weighted')
+    f1 = f1_score(y_test, y_predicted, average='weighted')
+    return accuracy, precision, recall, f1
